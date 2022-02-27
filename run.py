60100ae158e57cbd11beb615f78808a2525cf287
vanderpatrick <patrick123321123@icloud.com>
2022-02-27 10:08:09 +0000

    Create get_user_input to get data string from the user

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_user_input():
+    """
+    gets user input to choose between 3 menu options
+    """
+    print("Please enter the input of the desired option")
+    print("The input needs to be a number between 1 and 3 \n")
+
+    user_input = input("enter desired options here: ")
+    print(f"option was {user_input}")
+
+get_user_input()    
