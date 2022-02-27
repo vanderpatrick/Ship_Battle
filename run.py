760bb892cff30e10b9e7346d593e9bc0e3a01d03
vanderpatrick <patrick123321123@icloud.com>
2022-02-27 13:34:47 +0000

    Add return statment in all functions to call back function main

--- run.py
+++ run.py
@@ -20,20 +20,49 @@ def input_validation(user_input):
     if parsed not in valid_options:
         raise ValueError(
             f"Needs to be an value between 1, 2 or 3 you provided {user_input}"
-        )
-    
+        )    
     return parsed
 
 def start_game():
+    """
+    Function that start game.
+    """
     print('game started')
 
-def intructions():
-    print("some istructures")
 
-def credits():
-    print('made by patrick')
+def intructions():
+    """
+    Function that show user the instructions
+    """
+    print("some istructures")
+    print("some istructures")
+    print("some istructures\n")
+    print("1.main menu")
+    print("2.Start Game")
+    user_input = get_user_input()
+    if user_input == 1:
+        return main()
+    if user_input == 2:
+        return start_game()
+
+
+def credits_for_user():
+    """
+    function that shows user the credits
+    """
+    print('made by patrick\n')
+    print('1.Main menu')
+    print('2.Start Game')
+    user_input = get_user_input()
+    if user_input == 1:
+        return main()
+    if user_input == 2:
+        return start_game()
 
 def main():
+    """
+    function to call all functions
+    """
     print("Please enter the input of the desired option")
     print("The input needs to be a number between 1 and 3 \n")
     print("1.start Game")
@@ -46,4 +75,5 @@ def main():
         return intructions()
     if user_input == 3:
         return credits()
+
 main()
