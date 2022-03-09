1d53341430309be008f1f06835844545f86baecd
vanderpatrick <patrick123321123@icloud.com>
2022-03-09 22:53:08 +0000

    Add call board and count hit functions to start_game

--- run.py
+++ run.py
@@ -49,6 +49,22 @@ def start_game():
     computer_guess = [[" "] * 8 for i in range(8)]
     letters_translation = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
+    def call_board(board):
+        print('  A B C D E F G H')
+        print('  ---------------')
+        row_number = 1
+        for row in board:
+            print('%d|%s|' % (row_number, '|'.join(row)))
+            row_number += 1
+
+    def count_hits(board):
+        count = 0
+        for row in board:
+            for column in row:
+                if column == "X":
+                    count += 1
+        return count
+
 
 def intructions():
     """
