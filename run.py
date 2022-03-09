1996f718d5d502e06de6094632598eaca27e980e
vanderpatrick <patrick123321123@icloud.com>
2022-03-09 21:49:49 +0000

    Remove start_game(function)

--- run.py
+++ run.py
@@ -37,69 +37,7 @@ def start_game():
     """
     Function that start game.
     """
-    player_place_board = [[' '] * 8 for x in range(8)]
-    computer__place_board = [[' '] * 8 for x in range(8)]
-    letters_field = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
-
-
-    def print_board(board):
-        print('  A B C D E F G H')
-        print('  ---------------')
-        row_number = 1
-        for row in board:
-            print('%d|%s|' % (row_number, '|'.join(row)))
-            row_number += 1
-
-    def create_ships(board):
-        for ship in range(5):
-            ship_row, ship_column = randint(0, 7), randint(0, 7)
-            while board[ship_row][ship_column] == 'X':
-                ship_row, ship_column = randint(0, 7), randint(0, 7)
-            board[ship_row][ship_column] = 'X'
-
-
-    def get_ship_location():
-        row = input('Please enter row from 1 - 8: ')
-        while row not in '12345678':
-            print('Enter valid row')
-            row = input('Please enter row from 1 - 8: ').upper()
-        column = input('Please enter column from a - h: ').upper()
-        while column not in 'ABCDEFGH':
-            print('Enter valid column')
-            column = input('Please enter column from a - h: ').upper()
-        return int(row) - 1, letters_field[column]
-
-    def count_hit_ships(board):
-        count = 1
-        for row in board:
-            for column in row:
-                if column == 'X':
-                    count += 1
-        return count
-
-    create_ships(computer__place_board)
-    turns = 3
-    while turns > 0:
-        print('welcome to battleship')
-        print_board(player_place_board)
-        row, column = get_ship_location()
-        if player_place_board[row][column] == '-':
-            print('you already guessed that')
-        elif player_place_board[row][column] == 'X':
-            print('congratulations u hit the shit')
-            player_place_board[row][column] = 'X'
-            turns -= 1
-        else:
-            print('missed')
-            player_place_board[row][column] = '-'
-            turns -= 1
-        if count_hit_ships(player_place_board) == 5:
-            print('sunk alles')
-            break
-        print(f'you have {turns} remaning')
-        if turns == 0:
-            print('gameOver')
-            break
+    
 
 
 def intructions():
